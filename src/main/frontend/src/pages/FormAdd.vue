1fb11b4bf3dca972bb39c37f290f28e4d4ab34ff
Enol-Igareta <enol1code@gmail.com>
2023-02-10 13:21:16 +0100

    frontend

--- src/main/frontend/src/pages/FormAdd.vue
+++ src/main/frontend/src/pages/FormAdd.vue
@@ -7,18 +7,7 @@ import axios from "axios";
 const router = useRouter();
 
 const $q = useQuasar();
-const redirectToLogin = () => {
-  router.push({ path: "/list" });
-};
 
-const userForm = ref({
-  name: "",
-  // date: "",
-  // type: "",
-  // family: "",
-  // country: "",
-  // gender: "",
-});
 let nameModel = ref("");
 let typeModel = ref("");
 let genderModel = ref("");
@@ -26,6 +15,7 @@ let familyModel = ref("");
 let countryModel = ref("");
 
 const onSubmit =  async() => {
+
 
     axios({
       method: 'POST',
@@ -37,45 +27,45 @@ const onSubmit =  async() => {
         gender: genderModel.value,
         country: countryModel.value,
         family: familyModel.value
-        // type:{
-        //   "type": typeModel.value
-        // },
-        // gender:{
-        //   "gender": genderModel.value
-        // },
-        // type:{
-        //   "type": typeModel.value
-        // },
       }
     })
-      .then(res=>console.log(res))
-      .catch(err=>console.log(err))
-    };
-
+      .then(res=> $q.notify({
+      type: 'positive',
+      message: "Animal added successfully!",
+      icon: "fa-solid fa-circle-check"
+    })
+    )
+    .catch(err=>console.log(err))
+    onReset();
+  };
 
 const onReset = () => {
-  userForm.value = {
-    name: "",
-    // password1: "",
-    // password2: "",
-    // conditions: false,
-    // errorConditions: false,
-  };
+    nameModel.value ="";
+    typeModel.value ="";
+    genderModel.value ="";
+    countryModel.value ="";
+    familyModel.value ="";
 };
 </script>
 <template>
   <q-page class="row">
+    <div class="q-pa-md q-gutter-sm bg-grey-3 col-12">
+    <q-breadcrumbs>
+      <q-breadcrumbs-el icon="fa-solid fa-house-circle-xmark" to="/" style="font-size:16px;" />
+      <q-breadcrumbs-el label="List" icon="fa-solid fa-list" to="/list" />
+    </q-breadcrumbs>
+  </div>
     <div class="row col-12 items-center">
       <q-img
         src="https://images.moviesanywhere.com/cac313fcf9373e9446b6bb8ff171b8d1/8b8d6a81-b8e6-49ad-b835-32c2f01a03c5.jpg"
         spinner-color="white"
         class="col-0 col-xl-7 col-lg-7 col-md-7"
-        style="height: 100vh"
+        style="height: 94vh"
       />
       <q-form
         @submit="onSubmit"
         @reset="onReset"
-        class="column q-gutter-xl col-10 col-md-4 col-lg-4 col-xl-4 q-pl-xl"
+        class="column q-gutter-lg col-10 col-md-4 col-lg-4 col-xl-4 q-pl-xl"
       >
         <span class="text-h1 text-weight-bold">Add Animal</span>
         <q-input
@@ -83,7 +73,10 @@ const onReset = () => {
           v-model="nameModel"
           label="Name"
           type="text"
-          lazy-rules
+          lazy-rules="ondemand"
+          :rules="[
+            (val) => (val && val.length > 3) || 'This field is required',
+          ]"
         />
 
         <q-input
@@ -91,25 +84,10 @@ const onReset = () => {
           v-model="typeModel"
           label="Type"
           type="text"
-          lazy-rules
-
-        />
-
-        <q-input
-          filled
-          v-model="genderModel"
-          label="Gender"
-          type="text"
-          lazy-rules
-
-        />
-
-        <q-input
-          filled
-          v-model="countryModel"
-          label="country"
-          type="text"
-          lazy-rules
+          lazy-rules="ondemand"
+          :rules="[
+            (typeModel) => (typeModel && typeModel.length > 3) || 'This field is required',
+          ]"
 
         />
 
@@ -118,9 +96,37 @@ const onReset = () => {
           v-model="familyModel"
           label="family"
           type="text"
-          lazy-rules
+          lazy-rules="ondemand"
+          :rules="[
+            (val) => (val && val.length > 3) || 'This field is required',
+          ]"
 
         />
+
+        <q-input
+          filled
+          v-model="genderModel"
+          label="Gender"
+          type="text"
+          lazy-rules="ondemand"
+          :rules="[
+            (val) => (val && val.length > 3) || 'This field is required',
+          ]"
+
+        />
+
+        <q-input
+          filled
+          v-model="countryModel"
+          label="country"
+          type="text"
+          lazy-rules="ondemand"
+          :rules="[
+            (val) => (val && val.length > 3) || 'This field is required',
+          ]"
+
+        />
+
 
         <div class="column justify-end">
           <q-btn
--- src/main/frontend/src/pages/ListAnimals.vue
+++ src/main/frontend/src/pages/ListAnimals.vue
@@ -1,16 +1,19 @@
 <script setup>
-import { onMounted, ref, reactive } from "vue";
+import { onMounted, ref, reactive, onUpdated } from "vue";
 import axios from "axios";
-let datos = reactive("");
+
 const rows = ref([]);
+let selected = ref([]);
 onMounted(() => {
   axios
     .get("http://localhost:8080/api/animals")
     .then((response) => (rows.value = response.data));
-  rows.value.forEach((row, index) => {
-    row.index = index;
-  });
 });
+
+onUpdated(()=>{
+
+})
+
 
 const columns = [
   {
@@ -58,28 +61,34 @@ const columns = [
     align: "center",
     sortable: true,
   },
-  // { name: 'type', label: 'type', field: 'animals.gender', sortable: true, align: 'center',}
 ];
-
-// export default {
-//   setup () {
-//     return {
-//       columns,
-//       rows,
 
 pagination: ref({
   rowsPerPage: 10,
 });
-//     }
-// }
-// }
-</script>
-<template>
-  <!-- <div v-for="post in datos" :key="post.id">
-    <h2>{{ post.body }}</h2>
-  </div> -->
 
-  <div class="q-pa-md">
+const deleteAnimal =  async() => {
+
+  axios.delete("http://localhost:8080/api/animals/" + selected.value[0].id)
+  .then((res) => {
+      const index = rows.value.findIndex( row => row.id === selected.value[0].id);
+      rows.value.splice(index, 1);
+    })
+    .catch((err) => console.log(err));
+
+};
+</script>
+
+<template>
+<div class="q-pa-md q-gutter-sm bg-grey-3">
+    <q-breadcrumbs>
+      <q-breadcrumbs-el icon="fa-solid fa-house-circle-xmark" to="/" style="font-size:16px;" />
+      <q-breadcrumbs-el label="Add Animal" icon="fa-solid fa-user-plus" to="/formAdd" />
+    </q-breadcrumbs>
+  </div>
+
+
+  <div class="column q-pa-md ">
     <q-table
       title="Animales"
       :rows="rows"
@@ -87,7 +96,15 @@ pagination: ref({
       row-key="name"
       dark
       color="amber"
+      selection="single"
+      v-model:selected="selected"
+      class="col-8"
     />
+    <q-btn-group class="self-end q-my-lg">
+      <q-btn color="amber" rounded glossy icon="fa-solid fa-trash-can" @click="deleteAnimal" />
+      <q-btn color="amber" rounded glossy icon-right="fa-solid fa-pencil"  />
+    </q-btn-group>
+
   </div>
 </template>
 
