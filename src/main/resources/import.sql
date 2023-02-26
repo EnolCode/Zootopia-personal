07b5a6705784a697190ab2b1f0b6d6d422435a8d
Enol-Igareta <enol1code@gmail.com>
2023-02-26 12:58:20 +0100

    import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,23 +12,11 @@ INSERT INTO animals (id,date,name,gender) VALUES (default,'2008-12-01','whyski',
 INSERT INTO countries (id_country,country) VALUES (default,'Spain')
 
 
-<<<<<<< HEAD
--- INSERT INTO genders (id_gender, gender) VALUES (default,'Female')
-
--- INSERT INTO types (id_type, type) VALUES (default,'Cocodrile')
-
--- UPDATE animals SET family_id = 1
--- UPDATE animals SET country_id = 1
--- UPDATE animals SET gender_id = 1
--- UPDATE animals SET type_id = 1
-=======
-
 INSERT INTO types (id_type, type) VALUES (default,'Cocodrile')
 
 
 UPDATE animals SET country_id = 1
 UPDATE animals SET type_id = 1
->>>>>>> relaciones
 
 
   
