892b6b92a337b5735d5437f96c1e22ab012c0406
Enol-Igareta <enol1code@gmail.com>
2023-03-08 21:19:05 +0100

    borrada relacion familia

--- src/main/java/com/zootopia/demo/repository/FamilyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zootopia.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zootopia.demo.entity.Family;
-
-public interface FamilyRepository extends JpaRepository<Family, Long> {
-    
-}
